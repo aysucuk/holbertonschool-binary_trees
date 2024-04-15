4269dd5e2ff768020337cc2333c185088741ff59
Akuro1411 <nahidisayev147@gmail.com>
2024-04-15 06:57:33 +0000

    first free then equal null

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,30 +1,27 @@
 #include "binary_trees.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_delete - deletes a node from tree
+ * @tree: - the node is entered for deleting
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-  if (tree != NULL)
-  {
-    if (tree->parent != NULL)
-    {
-      tree->parent = NULL;
-      free(tree->parent);
-    }
-
-    if (tree->right != NULL)
-    {
-      tree->right = NULL;
-      free(tree->right);
-    }
-
-    if (tree->left != NULL)
-    {
-      tree->left = NULL;
-      free(tree->left);
-    }
-    free(tree);
-  }
+	if (tree != NULL)
+	{
+		if (tree->parent != NULL)
+		{
+			free(tree->parent);
+			tree->parent = NULL;
+		}
+		if (tree->right != NULL)
+		{
+			free(tree->right);
+			tree->right = NULL;
+		}
+		if (tree->left != NULL)
+		{
+			free(tree->left);
+			tree->left = NULL;
+		}
+		free(tree);
+	}
 }

d983c37a4ead32396550133cbee632c45534e243
Akuro1411 <nahidisayev147@gmail.com>
2024-04-15 11:10:44 +0000

    code is designed for betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_inorder - prints each node in order
+ * @tree: - the root node of bt
+ * @func: - the function for input
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-  if (tree != NULL && func != NULL)
-  {
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
-  }
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
 }

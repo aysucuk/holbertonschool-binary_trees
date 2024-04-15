0228152d3bc90d5ba3ee0ff1d8fa164c1a92ccc4
Akuro1411 <nahidisayev147@gmail.com>
2024-04-15 06:17:27 +0000

    else is deleted

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	else
+
+	if (parent != NULL)
 	{
 		if (parent->left == NULL)
 			parent->left = new_node;
